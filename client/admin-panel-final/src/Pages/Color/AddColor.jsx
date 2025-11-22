dd7e2b9c838383313f589a54e005b148d1f04403
PradeepRai218 <pradeep.rai@wscubetech.com>
2025-11-22 19:45:50 +0530

    latest Code

---
+++ client/admin-panel-final/src/Pages/Color/AddColor.jsx
@@ -0,0 +1,107 @@
+import React, { useEffect, useState } from "react";
+import { useForm } from "react-hook-form";
+import { ChromePicker } from "react-color";
+import { Link, useNavigate, useParams } from "react-router-dom";
+import axios from "axios";
+
+  import { ToastContainer, toast } from 'react-toastify';
+export default function AddColor() {
+
+  let apiBaseUrl = import.meta.env.VITE_APIBASE;
+
+
+  const [color, setColor] = useState("#000000");
+  const { id: updateId } = useParams();
+  const updateIdState = Boolean(updateId);
+
+ 
+  const handleColorChange = (newColor) => {
+    setColor(newColor.hex);
+  };
+
+ let  navigate=useNavigate()
+ 
+
+  const onSubmit = (e) => {
+    e.preventDefault()
+    let obj={
+      colorName:e.target.colorName.value,
+      colorCode:color,
+      colorOrder:e.target.colorOrder.value //1
+    }
+
+    axios.post(`${apiBaseUrl}/color/create`,obj)
+    .then((res)=>res.data)
+    .then((finalRes)=>{
+       if(finalRes._status){
+          toast.success(finalRes._message)
+          setTimeout(()=>{
+            navigate('/color/view')
+          },2000)
+       }
+       else{
+          toast.error(finalRes.error.colorName)
+       }
+        // finalRes._status  == True
+    })
+
+  };
+
+  return (
+    <div className="w-full">
+      <ToastContainer/>
+      <div className="max-w-[1220px] mx-auto py-5">
+        <h3 className="text-[20px] font-semibold bg-slate-100 py-2 px-3 rounded-t-md border border-slate-400">
+          {updateIdState ? "Update Color" : "Add Colors"}
+        </h3>
+
+        <form
+        onSubmit={onSubmit}
+          className="p-3 border border-t-0 rounded-b-md border-slate-400"
+         
+        >
+          {/* Color Name */}
+          <div className="mb-5">
+            <label className="block text-md font-medium text-gray-900">Color Name</label>
+            <input
+              type="text"
+              name="colorName"
+              className="text-[19px] border-2 shadow-sm border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full py-2.5 px-3"
+              placeholder="Enter Color Name"
+            />
+            
+          </div>
+
+          {/* Color Picker */}
+          <div className="mb-5">
+            <label className="block text-md font-medium text-gray-900">Color Picker</label>
+            <div className="flex items-center gap-3">
+              <ChromePicker color={color} onChange={handleColorChange} />
+              <div className="w-10 h-10 border border-gray-400 rounded-md" style={{ backgroundColor: color }}></div>
+            </div>
+          </div>
+
+          {/* Color Order */}
+          <div className="mb-5">
+            <label className="block text-md font-medium text-gray-900">Order</label>
+            <input
+              type="number"
+              name="colorOrder"
+              className="text-[19px] border-2 shadow-sm border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full py-2.5 px-3"
+              placeholder="Enter Order"
+            />
+          
+          </div>
+
+          {/* Submit Button */}
+          <button
+            type="submit"
+            className="focus:outline-none my-10 text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900"
+          >
+            {updateIdState ? "Update Color" : "Add Color"}
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+}
